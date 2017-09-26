3b31134df3fcb59bbe03ce9990b7954aac1986f3
Egor Stepanov <epstepanov@yamoney.ru>
2017-09-26 19:12:48 +0300

    initial commit

---
+++ B/src/B.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+@SuppressWarnings("ALL")
+public class B {
+
+    static class ProblemSolver {
+        public void solveTheProblem(InputReader in,PrintWriter out){
+
+        }
+    }
+
+
+
+    //Default template for all the codes
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream),32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+    }
+
+    //Main method for all the codes
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+
+        ProblemSolver problemSolver = new ProblemSolver();
+        problemSolver.solveTheProblem(in, out);
+        out.close();
+    }
+
+}
